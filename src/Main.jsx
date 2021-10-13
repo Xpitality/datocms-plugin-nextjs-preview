5b3cfb4123763eff9e6a3413d367944ecbeb1897
vishalshah6153 <vishal@xpitality.com>
2021-10-13 12:02:36 +0530

    Removed default language from URL

--- src/Main.jsx
+++ src/Main.jsx
@@ -98,8 +98,8 @@ export default class Main extends Component {
     const path = this.getEntityPath();
     const noSlashInstanceUrl = instanceUrl.replace(/\/$/, '');
 
-    const previewHref = `${noSlashInstanceUrl}${previewPath}?slug=${path}${previewSecret ? `&secret=${previewSecret}` : ''}${selectedLocale ? `&locale=${selectedLocale}` : ''}`;
-    const liveHref = `${noSlashInstanceUrl}/${selectedLocale}/${path}`;
+    const previewHref = `${noSlashInstanceUrl}${previewPath}?slug=${path}${previewSecret ? `&secret=${previewSecret}` : ''}${selectedLocale && selectedLocale !== 'en' ? `&locale=${selectedLocale}` : ''}`;
+    const liveHref = `${noSlashInstanceUrl}${selectedLocale && selectedLocale !== 'en' ? `/${selectedLocale}` : ''}/${path}`;
 
     return (
       <>
